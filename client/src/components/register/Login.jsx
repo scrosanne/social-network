f0b90e646e77ed2d0832cf08d4784ba96f179239
Rosanne Schönig <rosanne.schoenig@gmx.de>
2023-01-10 10:20:54 +0100

    removed unused class component comment

--- client/src/components/register/Login.jsx
+++ client/src/components/register/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
@@ -60,79 +60,3 @@ export default function Login() {
         </div>
     );
 }
-
-// // export default class Login extends Component {
-//     // constructor(props) {
-//     //     super(props);
-
-//     //     this.state = {
-//     //         error: false,
-//     //     };
-
-//     //     //bind methods
-//     //     this.handleInputChange = this.handleInputChange.bind(this);
-//     //     this.handleSubmit = this.handleSubmit.bind(this);
-//     // }
-
-//     // m e t h o d s
-// //     handleInputChange(e) {
-// //         const text = e.currentTarget.value;
-// //         this.setState({
-// //             [e.currentTarget.name]: text, //grab input element and add with propertyname to state object
-// //         });
-// //     }
-
-// // handleSubmit() {
-// //     //console.log(this.state);
-// //     //fetch POST request to the server's registration route
-// //     fetch("/login", {
-// //         method: "POST",
-// //         body: JSON.stringify(this.state), //stringify object with form input
-// //         headers: { "Content-Type": "application/json" },
-// //     })
-// //         .then((res) => res.json())
-// //         .then((response) => {
-// //             console.log("response", response);
-// //             if (response.success) {
-// //                 //"success true"
-// //                 location.replace("/");
-// //             } else {
-// //                 this.setState({ error: true });
-// //                 //"success false"
-// //                 //update state to make error appear, also for catch
-// //             }
-// //         });
-// // }
-
-//     // t e m p l a t e
-//     render() {
-//         return (
-//             <div className="register">
-//                 <h1>log in</h1>
-//                 {this.state.error === true && <p>smth wrong</p>}
-
-//                 <div className="form-block">
-//                     <input
-//                         type="email"
-//                         name="email"
-//                         placeholder="Email"
-//                         onChange={this.handleInputChange}
-//                     />
-
-//                     <input
-//                         type="password"
-//                         name="password"
-//                         placeholder="Password"
-//                         onChange={this.handleInputChange}
-//                     />
-
-//                     <button onClick={this.handleSubmit}>Log in</button>
-//                     <p>
-//                         forgot password? &nbsp;
-//                         <Link to="/reset">reset</Link>
-//                     </p>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
